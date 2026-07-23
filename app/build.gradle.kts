6255c1a2b5dd65288c513e78a7353aae6d7c3fde
abuhamam2015929-netizen <abuhamam2015929@gmail.com>
2026-07-23 05:52:19 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
 
     // Firebase (BoM يدير الإصدارات تلقائياً)
     implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
