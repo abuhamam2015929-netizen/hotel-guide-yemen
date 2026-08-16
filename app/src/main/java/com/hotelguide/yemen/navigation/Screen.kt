2340d46bdac8f9947a765eaf28298b6f1d6b5f41
abuhamam2015929-netizen <abuhamam2015929@gmail.com>
2026-08-16 05:32:06 +0300

    Update Screen.kt

--- app/src/main/java/com/hotelguide/yemen/navigation/Screen.kt
+++ app/src/main/java/com/hotelguide/yemen/navigation/Screen.kt
@@ -11,8 +11,8 @@ sealed class Screen(val route: String) {
     data object RoomDetail : Screen("room-detail/{roomId}") {
         fun createRoute(roomId: String) = "room-detail/$roomId"
     }
-    data object Payment : Screen("payment/{roomId}") {
-        fun createRoute(roomId: String) = "payment/$roomId"
+    data object Payment : Screen("payment/{hotelId}/{roomId}") {
+        fun createRoute(hotelId: String, roomId: String) = "payment/$hotelId/$roomId"
     }
     data object AdminLogin : Screen("admin-login")
     data object AdminDashboard : Screen("admin-dashboard")
